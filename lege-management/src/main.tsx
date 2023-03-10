e1749fe4a6e597ea92aa2330ac4ea380a1b3bec2
mary13144 <qq1427183122@gmail.com>
2023-03-10 15:16:26 +0800

    test

--- lege-management/src/main.tsx
+++ lege-management/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //初始化样式一般放在最前面
 import 'reset-css'
+//UI组件库样式
+//组件样式
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
